a6f3f38ad77134a5ba121bda005cff60036dc344
Jasmine Indira Wibowo <103538319+Jasmineindira@users.noreply.github.com>
2022-09-14 11:04:09 +0700

    Tugas 2 PBP

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
 # TODO: Create your views here.
+from django.shortcuts import render
+from katalog.models import CatalogItem
+
+# Views
+def show_katalog(request):
+    data_item_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_item_katalog,
+    'nama': 'Jasmine Indira Wibowo',
+    'id': '2106650374'
+    }
+    return render(request, "katalog.html", context)
